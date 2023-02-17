8b532aecbc108de6edfc45dceef467d4916eedee
Ricardo Machado <ricardo_cabista@hotmail.com>
2023-02-17 19:42:07 -0300

    feat: login route adicionada

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-
 import './App.css'
 
-function App() {
+// components
+import LoginPage from './pages/login/login.page'
 
+// utilities
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
