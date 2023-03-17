e0d9538740d4e04ecf91f3266bd75fe909330e90
Ricardo Machado <ricardo_cabista@hotmail.com>
2023-03-17 14:30:41 -0300

    feat: types and opitional icon added to custombutton

--- src/components/button/button.styled.ts
+++ src/components/button/button.styled.ts
@@ -1,9 +1,18 @@
 import styled from 'styled-components'
 
-export const CustonButton = styled.button`
+interface IiconsProps {
+  icon?: React.ReactNode
+}
+
+export const CustonButton = styled.button<IiconsProps>`
   background: #0095f6;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 258px;
+  font-weight: bold;
   padding: 0.6rem;
+  gap: 0.3rem;
   border: none;
   border-radius: 0.5rem;
   color: #ffffff;
--- src/components/button/custonButton.component.tsx
+++ src/components/button/custonButton.component.tsx
@@ -1,14 +1,21 @@
 import { CustonButton } from './button.styled'
-import { ButtonHTMLAttributes, FunctionComponent } from 'react'
+import React, { ButtonHTMLAttributes, FunctionComponent } from 'react'
 
 interface IbuttonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: string
+  icon?: React.ReactNode
 }
 
 const ButtonComponent: FunctionComponent<IbuttonProps> = ({
   children,
+  icon,
   ...rest
 }) => {
-  return <CustonButton {...rest}>{children}</CustonButton>
+  return (
+    <CustonButton {...rest}>
+      {icon && <div>{icon}</div>}
+      {children}
+    </CustonButton>
+  )
 }
 export default ButtonComponent
