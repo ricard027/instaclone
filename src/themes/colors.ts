fcc892469bf9144d75186b9faca8f5c65993848f
Ricardo Machado <ricardo_cabista@hotmail.com>
2023-03-31 08:23:11 -0300

    style: add aditional colors

--- src/themes/colors.ts
+++ src/themes/colors.ts
@@ -1,11 +1,18 @@
 const Colors = {
+  bg: {
+    primary: '#121212'
+  },
   input: {
     primary: '#0095f6'
   },
   Button: {
     primary: '#0095f6'
   },
-  Error: '#ea4335'
+  Error: '#ea4335',
+  Customs: {
+    Red: '#ff3040',
+    Blue: '#0095f6'
+  }
 }
 
 export default Colors
