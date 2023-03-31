c6e9350a8c5513d82e78a2866614a2f469fc6906
Ricardo Machado <ricardo_cabista@hotmail.com>
2023-03-31 08:25:23 -0300

    feat: add coments template and styles to publication component

--- src/components/publication/publication.component.tsx
+++ src/components/publication/publication.component.tsx
@@ -1,31 +1,64 @@
+import { FunctionComponent } from 'react'
+// Style
 import {
+  ComentsArea,
   IconsArea,
+  LikePuclicationQtd,
   Publication,
-  PublicationContainer
+  PublicationContainer,
+  UsercontainerPublication
 } from './publication.style'
 
+// Icons
 import { AiOutlineHeart } from 'react-icons/ai'
 import { TbMessageCircle2, TbSend } from 'react-icons/tb'
+import { FaRegBookmark } from 'react-icons/fa'
 
-const PublicationComponent = () => {
+interface IpublicationProps {
+  images: string[]
+}
+const PublicationComponent: FunctionComponent<IpublicationProps> = ({
+  images
+}) => {
+  console.log(images)
   return (
     <PublicationContainer>
-      <div>
-        <div>
-          <img src="" alt="" />
-          <p>Name user</p>
+      <UsercontainerPublication>
+        <div className="user-content">
+          <div className="image-container"></div>
+          <strong>Name user</strong>
           <span>2h</span>
         </div>
-        <div>...</div>
-      </div>
+        <div className="dots">...</div>
+      </UsercontainerPublication>
       <Publication>
         <div>{/* img */}</div>
       </Publication>
       <IconsArea>
-        <AiOutlineHeart size={26} />
-        <TbMessageCircle2 size={26} />
-        <TbSend size={26} />
+        <div>
+          <AiOutlineHeart size={26} />
+          <TbMessageCircle2 size={26} />
+          <TbSend size={26} />
+        </div>
+        <div>
+          <FaRegBookmark size={26} />
+        </div>
       </IconsArea>
+      <LikePuclicationQtd>1.456 curtidas</LikePuclicationQtd>
+      <ComentsArea>
+        <div className="description-publication">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+          vitae, quidem autem molestiae repudiandae vero nostrum ex, laborum ea
+          sequi fuga suscipit cumque... <span className="view-more">mais</span>
+        </div>
+        <span className="traduction">Ver tradução</span>
+        <span className="view-all-coments">Ver todos os 24 comentários</span>
+        <div>
+          <strong>User exemple </strong>
+          <span>comentário do user</span>
+        </div>
+        <div className="add-coments">Adicione um comentário </div>
+      </ComentsArea>
     </PublicationContainer>
   )
 }
--- src/components/publication/publication.style.ts
+++ src/components/publication/publication.style.ts
@@ -1,22 +1,91 @@
 import styled from 'styled-components'
+import Colors from '../../themes/colors'
+
+const { primary } = Colors.bg
 
 export const PublicationContainer = styled.div`
   width: 70%;
-  border: solid red;
-  margin: 0 auto;
+  background: ${primary};
+  border-bottom: solid 1px #363636;
+  margin: 1rem auto;
 `
 export const Publication = styled.section`
   margin: 0 auto;
   display: flex;
   flex-direction: column;
-  height: 80vh;
+  height: 60vh;
   width: 90%;
-  border: solid red;
-  background: #000;
+  background: #363636;
+  border-radius: 0.3rem;
+`
+export const UsercontainerPublication = styled.div`
+  margin: 0 auto;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 3rem;
+  width: 90%;
+  color: #f4f4f4;
+
+  .user-content {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+  .image-container {
+    border-radius: 50%;
+    height: 2.2rem;
+    width: 2.2rem;
+    background: #363636;
+  }
+  .dots {
+    font-size: 1.5rem;
+    cursor: pointer;
+  }
 `
 export const IconsArea = styled.div`
   margin: 0 auto;
-  height: 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 2.5rem;
   width: 90%;
-  background: red;
+  svg {
+    cursor: pointer;
+    color: #fff;
+  }
+  div {
+    &:first-child {
+      svg {
+        margin-right: 1rem;
+      }
+    }
+  }
+`
+export const LikePuclicationQtd = styled.div`
+  width: 90%;
+  height: 1.5rem;
+  margin: 0 auto;
+  color: #fff;
+`
+export const ComentsArea = styled.div`
+  margin: 0 auto;
+  color: #fff;
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem 0;
+  gap: 0.5rem;
+
+  .add-coments,
+  .view-all-coments,
+  .view-more {
+    color: #8e8e8e;
+    cursor: pointer;
+  }
+
+  .traduction {
+    font-size: 0.8rem;
+  }
 `
