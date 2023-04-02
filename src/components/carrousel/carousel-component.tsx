4153d3ee54fc7484c25f62214e4884b8a8efdc21
Ricardo Machado <ricardo_cabista@hotmail.com>
2023-04-02 09:00:12 -0300

    fix: add styles swiper lib to make the carousel funcional

--- src/components/carrousel/carousel-component.tsx
+++ src/components/carrousel/carousel-component.tsx
@@ -2,6 +2,10 @@ import { FunctionComponent, ReactNode } from 'react'
 import { Swiper, SwiperProps } from 'swiper/react'
 import { Pagination, A11y, Navigation } from 'swiper'
 
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/scrollbar'
+import 'swiper/css/pagination'
 interface IcarouselProps extends SwiperProps {
   children: ReactNode
   configs: any
@@ -11,7 +15,11 @@ export const CarouselComponent: FunctionComponent<IcarouselProps> = ({
   configs
 }) => {
   return (
-    <Swiper modules={[Pagination, A11y, Navigation]} {...configs}>
+    <Swiper
+      modules={[Pagination, A11y, Navigation]}
+      slidesPerGroup={5}
+      cssMode={true}
+      {...configs}>
       {children}
     </Swiper>
   )
