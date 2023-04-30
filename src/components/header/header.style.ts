65d787ba0b4d16c8d34eee697ab2bd72d83ad311
Ricardo Machado <ricardo_cabista@hotmail.com>
2023-04-29 21:07:05 -0300

    add searchIcon with styled components

--- src/components/header/header.style.ts
+++ src/components/header/header.style.ts
@@ -1,6 +1,17 @@
 import styled from 'styled-components'
 import Colors from '../../themes/colors'
+import { AiOutlineSearch } from 'react-icons/ai'
 
+export const SearchIcon = styled(AiOutlineSearch)`
+  color: #fff;
+  position: absolute;
+  transition: opacity 0.2s;
+  z-index: 1;
+  transform: translatex(20%);
+  &.hide {
+    opacity: 0;
+  }
+`
 export const HeaderContainerMobile = styled.nav`
   position: fixed;
   width: 100%;
@@ -16,46 +27,41 @@ export const HeaderContainerMobile = styled.nav`
 
   @media screen and (max-width: 768px) {
     display: flex;
-    :last-child {
-      bottom: 0;
-      background-color: red;
-    }
   }
-  
+
   h1 {
     font-family: 'Lobster Two', cursive;
     color: #fff;
-    margin-left: 1rem;
+    margin-left: 5%;
   }
   div {
     display: flex;
     align-items: center;
-    gap: 1rem;
+    gap: 5%;
     margin-right: 1.5rem;
 
     input {
-      width: 15rem;
+      width: 50vw;
       height: 2rem;
       border-radius: 0.4rem;
       border: none;
-      font-size: 0.8em;
+      font-size: 1em;
       padding-left: 1rem;
-      position: relative;
       display: flex;
       background-color: #666;
+      color: #fff;
+
+      &:focus ~ #searchIcon {
+        display: none;
+      }
 
       ::placeholder {
         color: #fff;
         padding-left: 1rem;
       }
     }
-    svg {
+    .heartIcon {
       cursor: pointer;
-      :first-child {
-        position: absolute;
-        z-index: 1;
-        margin-left: 0.5rem;
-      }
     }
   }
 `
